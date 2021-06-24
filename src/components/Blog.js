37cd5a5d0949443d56430d65f441b0890622ea8e
Andrew Huynh <andrew.huynh17@gmail.com>
2021-06-23 22:30:31 -0400

    code clean up in blog requests, additional info for blogs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,56 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import axios from "axios";
+import React, { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [info, setInfo] = useState(false);
+  const [likes, setLikes] = useState(blog.likes);
+
+  const toggleInfo = () => {
+    setInfo(!info);
+    console.log(likes);
+  };
+
+  const handleLike = async () => {
+    const res = await axios.put(`/api/blogs${blog.id}`, { likes: likes + 1 });
+    setLikes(likes + 1);
+  };
+
+  const MoreInfoJSX = () => {
+    if (info) {
+      return (
+        <div>
+          <div>
+            {blog.title} by {blog.author}
+            <button onClick={toggleInfo}> More </button>)
+          </div>
+          <div>{blog.url}</div>
+          <div>
+            {blog.likes}
+            <button onClick={handleLike}>Like</button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {blog.title} by {blog.author}
+          <button onClick={toggleInfo}> More </button>
+        </div>
+      );
+    }
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+    width: "25%",
+    borderRadius: 5,
+  };
+
+  return <div style={blogStyle}>{MoreInfoJSX()}</div>;
+};
+
+export default Blog;
